ed8ed7b54d1902bd5b261d33eb984687f86ed016
tehnczon <147896311+tehnczon@users.noreply.github.com>
2025-04-18 20:08:49 +0800

    Update axios.js

--- frontendjoborder/src/api/axios.js
+++ frontendjoborder/src/api/axios.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 axios.defaults.withCredentials = true;
 axios.defaults.withXSRFToken = true;
-axios.defaults.baseURL = 'https://home.tehnczon.online/api';
+axios.defaults.baseURL = 'https://home.tehnczon.online';
 
 export default axios;
