8da629c87417f328fb9b8d25f6c7bfa27f886ab1
tehnczon <tehnczonlanticse@gmail.com>
2025-04-22 15:50:30 +0800

    with domain

--- frontendjoborder/src/api/axios.js
+++ frontendjoborder/src/api/axios.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 axios.defaults.withCredentials = true;
 axios.defaults.withXSRFToken = true;
-axios.defaults.baseURL = 'http://localhost:8001';
+axios.defaults.baseURL = 'https://desktop.tehnczon.online';
 
 export default axios;
